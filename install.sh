abb2222035d2dae75aecfa6acf3b4888efbb4cff
Marcel Jankrift <marcel@jankrift.de>
2019-12-27 12:04:33 +0100

    create contao user

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
-# create and switch user
-adduser contao
-usermod -aG sudo contao
+if [ $(id -u) -eq 0 ]; then
+    read -s -p "Enter contao user password: " password
+    pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+    useradd -m -p $pass contao -s /bin/bash
+    usermod -aG sudo contao
+    su -l contao
+fi
 
-su -l contao
+cd /home/contao
+mkdir -p .ssh
+touch .ssh/authorized_keys
